c0e74e3f7f3b8b9921f7c9613e0827ce163cdb67
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-14 20:40:15 +0530

    Create reverseString.java

---
+++ Day4_ReverseString/reverseString.java
@@ -0,0 +1,9 @@
+public class TechnologyEncroyable
+{
+	public static void main(String[] args) 
+	{
+		StringBuilder word = new StringBuilder( "Technology Encroyable"); 
+        String reversed = word.reverse().toString(); 
+        System.out.println(reversed);
+	}
+}
