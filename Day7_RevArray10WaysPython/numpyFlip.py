56129e81c8896912fd3d06132f5a6dc8d87fe402
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-17 19:58:02 +0530

    Create numpyFlip.py

---
+++ Day7_RevArray10WaysPython/numpyFlip.py
@@ -0,0 +1,10 @@
+# reverse array in python
+# different ways
+
+# 3. using np.flip()
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print(" Original : ",arr)
+rev = np.flip(arr)
+print(" Reversed : ",rev)
+
