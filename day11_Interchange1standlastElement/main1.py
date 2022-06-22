a93e079492986ca8b8a33c3fd57c71b3a499f7cf
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-22 20:31:49 +0530

    Update main1.py
    output ---- > [5,2,3,4,1]

--- day11_Interchange1standlastElement/main1.py
+++ day11_Interchange1standlastElement/main1.py
@@ -10,3 +10,58 @@ lst[size-1] = temp
 
 print("  New List: ",lst)
 
+
+# 2nd approcah - Interchanging using lst[-1] 
+
+lst = [1,2,3,4,5]
+
+lst[0], lst[-1] = lst[-1], lst[0]
+
+print("  New List: ",lst)
+
+
+
+
+# 3rd approcah - swap using a tuple
+
+list = [1,2,3,4,5]
+
+#storing in tuple
+tup = list[-1],list[0]
+
+#getback from tuple
+list[0], list[-1] = tup 
+
+print("  New List: ",list)
+
+
+
+
+# 4th approcah - *operand
+
+list = [1,2,3,4,5]
+
+start ,*middle , end = list
+
+list = [end, *middle, start]
+
+print(list)
+
+
+
+
+# 5th approcah - using pop() & insert()
+
+list = [1,2,3,4,5]
+
+start = list.pop(0)
+end = list.pop(-1)
+
+list.insert(0,end)
+list.append(start)
+
+
+print(list)
+
+
+
