95e26a3887fe95247e5797a27d3771d53e0d6ebe
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-23 19:14:29 +0530

    Create indexof.js

---
+++ Day12_CheckArrayContainsValue5Ways/indexof.js
@@ -0,0 +1,7 @@
+// Array.indexOf() 
+// returns index if found, else -1
+
+const languages = ['python', 'java', 'c#', 'js' , 'c++']
+languages.indexOf('c#') // 2
+languages.indexOf('python') // 0
+languages.indexOf('c') // -1
