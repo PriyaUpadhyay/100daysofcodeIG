16a534d4ddd33343b71cae53ea72459a955bf509
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-23 19:20:31 +0530

    Update indexof.js

--- Day12_CheckArrayContainsValue5Ways/indexof.js
+++ Day12_CheckArrayContainsValue5Ways/indexof.js
@@ -1,7 +1,7 @@
 // Array.indexOf() 
 // returns index if found, else -1
 
-const languages = ['python', 'java', 'c#', 'js' , 'c++']
+const languages = ['python', 'java', 'c#', 'js' , 'c++','python']
 languages.indexOf('c#') // 2
 languages.indexOf('python') // 0
 languages.indexOf('c') // -1
@@ -10,3 +10,7 @@ languages.indexOf('c') // -1
 languages.indexOf('c#',1) // 2
 languages.indexOf('c#',2) // 2
 languages.indexOf('c#',3) //-1
+
+
+// lastIndexOf() -- starts searching from the end 
+languages.lastIndexOf('python') //5
