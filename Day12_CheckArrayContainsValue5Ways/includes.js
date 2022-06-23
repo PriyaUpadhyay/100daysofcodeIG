45b7c44c2d2f460c312ff1e8872dc517284f1cd4
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-23 19:23:43 +0530

    Create includes.js

---
+++ Day12_CheckArrayContainsValue5Ways/includes.js
@@ -0,0 +1,7 @@
+// includes() -- returns true if present, else false
+const languages = ['python', 'java', 'c#', 'js' , 'c++','python']
+languages.includes('c#') // true
+
+
+// search from a desired index
+languages.includes('java',3) // false
