1af5e474ba38bb695c56609dcbba0acd59b62433
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2022-06-23 19:39:37 +0530

    Create some.js

---
+++ Day12_CheckArrayContainsValue5Ways/some.js
@@ -0,0 +1,9 @@
+// some() --  returns true if found,
+// else return false
+
+const languages = ['python', 'java', 'c#', 'js' , 'c++','python']
+languages.some(lang => lang === 'java') // true
+
+languages.some(lan => lang === 'c') // false
+
+
